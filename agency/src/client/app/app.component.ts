6b92ba587a381cb8029e2683a4cc33b8e4e17d26
Matthew Harwood <matthhar12@gmail.com>
2016-06-14 23:26:29 -0700

    - Added the base files for agency.

--- agency/src/client/app/app.component.ts
+++ agency/src/client/app/app.component.ts
@@ -1,6 +1,45 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+    <section class="parent primary">
+      <div class="child">
+        <h1 class="title">Tomato House</h1>
+        <p class="copy">
+          Ex congue debitis sed. At usu menandri iudicabit. Cu est integre nusquam, 
+          sint idque laoreet an nec. Id quo mentitum platonem, ea per diceret
+          pericula adversarium. Te iusto mentitum definitiones nam. Minim utinam
+          vel in, nusquam dolores duo ad. Est cu modus delicatissimi. Munere
+        </p>
+
+        <nav>
+          <ul>
+            <li><a href="" class="email">we@tomato.house</a></li>
+            <li><a href="">What we do</a></li>
+            <li><a href="" class="active">Who we are</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+    <section class="parent secondary">
+      <div class="child">
+        <h1 class="title">Tomato House</h1>
+        <p class="copy">
+          Ex congue debitis sed. At usu menandri iudicabit. Cu est integre nusquam, 
+          sint idque laoreet an nec. Id quo mentitum platonem, ea per diceret
+          pericula adversarium. Te iusto mentitum definitiones nam. Minim utinam
+          vel in, nusquam dolores duo ad. Est cu modus delicatissimi. Munere
+        </p>
+    
+        <nav>
+          <ul>
+            <li><a href="" class="email">we@tomato.house</a></li>
+            <li><a href="">What we do</a></li>
+            <li><a href="" class="active">Who we are</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+    `
 })
 export class AppComponent { }
